bb54ba330f2fac9d0a0aedfe65c28cc4d57196d0
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-19 20:23:57 -0300

    Update TelaInicial
    adjust in responsivity.

--- components/TelaInicial/index.js
+++ components/TelaInicial/index.js
@@ -7,39 +7,42 @@ import imgJogo from '../../assets/jogo-1.png';
 import imgXcloud from '../../assets/xcloud-1.png';
 import styles from './styles';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native';
 
 export default function TelaInicial (props) {
   return (
-   
-    <View style={ styles.container }>
-       <StatusBar hidden={true}/>
-      <Image source={ imgFundo } style={ styles.img } />
-      <Text style={ styles.titulo }>Xbox</Text>
-      <Text style={ styles.descricao }>Xbox é uma marca de consoles criada pela Microsoft. Toque nos cards para conhecer.</Text>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={ styles.container }>
+          <Image source={ imgFundo } style={ styles.img } />
+          <Text style={ styles.titulo }>Xbox</Text>
+          <Text style={ styles.descricao }>Xbox é uma marca de consoles criada pela Microsoft. Toque nos cards para conhecer.</Text>
 
-      <Pressable
-        onPress={ () => { props.navigation.navigate("Consoles") } }
-        style={styles.card}
-      >
-        <Image source={ imgConsole } style={ styles.cardImg } />
-        <Text style={ styles.cardTexto}>Conheça os consoles</Text>
-      </Pressable>
+          <Pressable
+            onPress={ () => { props.navigation.navigate("Consoles") } }
+            style={styles.card}
+          >
+            <Image source={ imgConsole } style={ styles.cardImg } />
+            <Text style={ styles.cardTexto}>Conheça os consoles</Text>
+          </Pressable>
 
-      <Pressable
-        onPress={ () => { props.navigation.navigate("Jogos") } }
-        style={styles.card}
-      >
-        <Image source={ imgJogo } style={ styles.cardImg } />
-        <Text style={ styles.cardTexto}>Conheça os jogos</Text>
-      </Pressable>
+          <Pressable
+            onPress={ () => { props.navigation.navigate("Jogos") } }
+            style={styles.card}
+          >
+            <Image source={ imgJogo } style={ styles.cardImg } />
+            <Text style={ styles.cardTexto}>Conheça os jogos</Text>
+          </Pressable>
 
-      <Pressable
-        onPress={ () => { props.navigation.navigate("Nuvem") } }
-        style={styles.card}
-      >
-        <Image source={ imgXcloud } style={ styles.cardImg } />
-        <Text style={ styles.cardTexto}>Conheça o xCloud</Text>
-      </Pressable>
-    </View>
+          <Pressable
+            onPress={ () => { props.navigation.navigate("Nuvem") } }
+            style={styles.card}
+          >
+            <Image source={ imgXcloud } style={ styles.cardImg } />
+            <Text style={ styles.cardTexto}>Conheça o xCloud</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>  
+    </>
   )
 }
--- components/TelaInicial/styles.js
+++ components/TelaInicial/styles.js
@@ -1,56 +1,63 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, PixelRatio } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 const styles = StyleSheet.create({
   container: {
     
-    flex: 1,
+    flex: 1 ,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: '#2c2e2d',
-    padding: 15,
+    padding: '1%',
+    
   },
   img: {
-    width: 80,
-    height: 80,
-    borderRadius: 100,
-    marginBottom: 10,
+    width: '23%',
+    height: '11%',
+    borderRadius: 50,
+    marginBottom: '5%',
   },
   titulo: {
-    fontSize:26,
+    fontSize:RFValue(26),
     fontWeight: "bold",
     color: '#107c0f',
-    marginBottom: 5,
+    marginBottom: '3%',
   },
   descricao: {
+   
     color: '#b2b2b2',
     textAlign: "center",
-    width: 320,
-    marginBottom: 25,
+    width: '93.92%',
+    marginBottom: '7%',
+ 
+   
+
   },
 
   card: {
-    width: '80%',
+    width: '75%',
     alignItems: "center",
     backgroundColor: '#107c0f',
     flexDirection: "row",
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginBottom: 20,
+    borderRadius: 15,
+    paddingVertical: '9%',
+    paddingHorizontal: '4%',
+    marginBottom: '5%',
   },
   
   cardImg: {
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    marginBottom: 10,
-    marginRight: 10,
+    width: '26%',
+    height: '140%',
+    borderRadius: 25,
+    marginTop: '10%',
+    marginBottom: '10%',
+    marginRight: '5%',
   },
   cardTexto: {
-    fontSize: 18,
+    fontSize:RFValue(17),
     color: '#fff',
     textAlign: "left",
-    width: 100,
+    width: '59%',
   },
 });
 
