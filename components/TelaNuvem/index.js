b9cb8ff5167807cc639d168fc49b9868e16ac217
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-19 20:22:05 -0300

    Create TelaNuvem

---
+++ components/TelaNuvem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Image, Text, Pressable } from 'react-native';
+
+
+export default function TelaNuvem() {
+  return (
+    <View >
+      <Text>
+        hellou
+      </Text>
+    </View>
+  )
+}
