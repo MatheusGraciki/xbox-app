8e5f3d30ea175a065a4e03fea0b13067a903624e
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-24 18:26:17 -0300

    Update TelaNuvem
    responsivity and conclusion of  screen

--- components/TelaNuvem/index.js
+++ components/TelaNuvem/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { View, Image, Text, Pressable } from 'react-native';
+import { ImageBackground, SafeAreaView } from 'react-native';
+import styles from './styles';
+import imageForeground from '../../assets/xcloud-2.png'
 
 
 export default function TelaNuvem() {
   return (
-    <View >
-      <Text>
-        hellou
-      </Text>
-    </View>
+  
+    
+        <ImageBackground
+          source={require('../../assets/fundo-xcloud.png')}
+          style={styles.imageBackground}
+        >
+          <SafeAreaView style={styles.container}>
+
+            <View>
+              <Text style={styles.titulo}>
+                Xbox Cloud Gaming
+              </Text>
+              <Text style={styles.subtitulo}>
+                Serviço de streaming de jogos
+              </Text>
+              <Text  style={styles.descricao}>
+                {'Jogue mais de 100 jogos de console de alta qualidade no seu dispositivo favorito.\n\nDesktop, Notebook, console ou até mesmo celular pode se tornar um XBOX.\n\nAtravés de uma assinatura você terá um XBOX virtual sempre que precisar.'}
+              </Text>
+
+              <Image style={styles.imageForeground} source={imageForeground}
+              />
+            </View>
+
+          
+          </SafeAreaView>
+        </ImageBackground> 
+    
+   
+      
+    
   )
 }
---
+++ components/TelaNuvem/styles.js
@@ -0,0 +1,65 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import { StyleSheet } from 'react-native'
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        
+        flex:0.75,
+     
+        
+        
+        
+    },
+    titulo: {
+       
+        fontSize:RFValue(26),
+        fontWeight: "bold",
+        color: '#107c0f',
+        marginBottom: '3%',
+        textAlign: 'center'
+    },
+      subtitulo: {
+       
+        color: '#b2b2b2',
+        fontWeight: 'bold',
+        textAlign: "center",
+        width: '99.92%',
+        marginBottom: '5%',
+        paddingHorizontal: 10,
+        fontSize:RFValue(19)
+    },
+    imageBackground: {
+        flex:1,
+        resizeMode:'cover',
+        justifyContent:'center',
+        alignItems: 'center',
+        alignSelf:'center'
+    },
+    imageForeground:{
+        marginTop:'7%',
+        width: '90%',
+        height: '45%', 
+       
+        alignSelf:'center',
+        justifyContent:'center',
+        resizeMode:'contain',
+      
+        
+
+    },
+    descricao: { 
+        
+        color: '#b2b2b2',
+        textAlign: 'center',
+        
+        paddingHorizontal: 10,
+        
+        marginTop: '5%',
+        fontSize: RFValue(17),
+    },
+
+})
+
+    export default styles
