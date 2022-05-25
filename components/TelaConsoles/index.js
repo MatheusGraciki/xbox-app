56faa481f9d134f6da6ad807766854e9475914a2
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-25 20:24:07 -0300

    Update TelaConsole

--- components/TelaConsoles/index.js
+++ components/TelaConsoles/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import { View, Image, Text, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import styles from './styles';
+
+
+import XboxSeriesS from '../../assets/XboxSeriesS.png'
+import XboxSeriesX from '../../assets/XboxSeriesX.png'
 
 
 export default function TelaConsole() {
   return (
-    <View >
-        <Text>
-        hellou
-        </Text>
-    </View>
+   
+      <SafeAreaView style={styles.container}>
+        
+          <Image source={XboxSeriesS} style={styles.xboxImage}
+          />
+          <Text style={styles.titulo}>
+            XBOX SERIES S
+          </Text>
+
+          <Text style={styles.subtitulo}>
+            Desempenho de última geração no menor Xbox de todos os tempos.
+          </Text>
+
+          <Text style={styles.descricao}>
+          O Series S é pra quem busca jogos de nova geração gastando pouco. é o console mais barato da nova geração. Ele é o equilibrio entre gráfico e performance.
+          </Text>
+
+          <Image source={XboxSeriesX} style={styles.xboxImage}
+          />
+
+          <Text style={styles.titulo}>
+            XBOX SERIES X
+          </Text>
+
+          <Text style={styles.subtitulo}>
+            O Xbox mais rápido e poderoso de todos os tempos.
+          </Text>
+
+          <Text style={styles.descricao}>
+            O Series X é pra quem quer poder absoluto. O console mais poderoso do mercado, excelência em gráficos e performance.
+          </Text>
+        
+      </SafeAreaView>
+        
+    
   )
 }
---
+++ components/TelaConsoles/styles.js
@@ -0,0 +1,51 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import { StyleSheet } from 'react-native'
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+            
+    },
+    titulo: {
+       
+        fontSize:RFValue(26),
+        fontWeight: "bold",
+        color: '#107c0f',
+        marginBottom: '2.5%',
+        textAlign: 'center'
+    },
+    subtitulo: {
+        
+        
+        textAlign: "center",
+        paddingHorizontal: 20,
+        marginBottom:'2.5%',
+        fontSize:RFValue(16),
+        fontWeight:'700'
+      
+
+      
+    },
+    descricao: {
+        fontWeight: '100',
+        textAlign: "center",
+        alignItems:'center',
+        marginBottom: '5%',
+        fontSize:RFValue(14),
+        padding:'3%' ,
+    },
+    xboxImage:{
+        width:100,
+        height:100,
+        resizeMode:'cover',
+       
+
+    }
+})
+
+export default styles
